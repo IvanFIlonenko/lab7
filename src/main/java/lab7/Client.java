249790758e2cb7997110bb6fe6340ce525edade3
Ivan <Ivan Filonenko>
2019-12-11 13:23:42 +0300

    Scripted auto-commit on change (2019-12-11 13:23:42) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -5,11 +5,13 @@ import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Client {
     private static Random rand        = new Random();
     public static void main(String[] args){
         try (ZContext context = new ZContext()) {
+            Scanner in = new Scanner(System.in);
             ZMQ.Socket worker = context.createSocket(SocketType.REQ);
 
             worker.connect("tcp://localhost:5671");
@@ -17,23 +19,25 @@ public class Client {
             int total = 0;
             while (true) {
                 //  Tell the broker we're ready for work
-                worker.send("Hi Boss");
+                String s = in.nextLine();
+                worker.send(s);
 
                 //  Get workload from broker, until finished
                 String workload = worker.recvStr();
-                boolean finished = workload.equals("Fired!");
-                if (finished) {
-                    System.out.printf("Completed: %d tasks\n", total);
-                    break;
-                }
-                total++;
+                System.out.println(workload);
+//                boolean finished = workload.equals("Fired!");
+//                if (finished) {
+//                    System.out.printf("Completed: %d tasks\n", total);
+//                    break;
+//                }
+//                total++;
 
                 //  Do some random work
-                try {
-                    Thread.sleep(rand.nextInt(500) + 1);
-                }
-                catch (InterruptedException e) {
-                }
+//                try {
+//                    Thread.sleep(rand.nextInt(500) + 1);
+//                }
+//                catch (InterruptedException e) {
+//                }
             }
         }
     }

de31476fabe975cf723e02cfb345b32d7daad484
Ivan <Ivan Filonenko>
2019-12-11 22:23:19 +0300

    Scripted auto-commit on change (2019-12-11 22:23:19) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -18,14 +18,16 @@ public class Storage
             worker.setHWM(0);
             worker.setIdentity("W".getBytes(ZMQ.CHARSET));
             worker.connect("tcp://localhost:5556");
+            //ZMQ.Poller poller = ctx.createPoller(1);
+            //poller.register(worker, ZMQ.Poller.POLLIN);
             long start = System.currentTimeMillis();
             while (!Thread.currentThread().isInterrupted()) {
-                if (System.currentTimeMillis() - start > 5000) {
-                    ZMsg msg = new ZMsg();
-                    msg.addString(left + "-" + right);
-                    msg.send(worker);
-                    start = System.currentTimeMillis();
-                }
+//                if (System.currentTimeMillis() - start > 5000) {
+//                    ZMsg msg = new ZMsg();
+//                    msg.addString(left + "-" + right);
+//                    msg.send(worker);
+//                    start = System.currentTimeMillis();
+//                }
                 ZMsg msg = ZMsg.recvMsg(worker);
                 ZFrame content = msg.getLast();
                 String s = content.toString();

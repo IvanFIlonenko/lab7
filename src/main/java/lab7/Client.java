f81ad7e94e4d6037f5d7b463c19e390c848649fb
Ivan <Ivan Filonenko>
2019-12-11 13:18:38 +0300

    Scripted auto-commit on change (2019-12-11 13:18:38) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -1,7 +1,37 @@
 package lab7;
 
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+
 public class Client {
     public static void main(String[] args){
+        try (ZContext context = new ZContext()) {
+            ZMQ.Socket worker = context.createSocket(SocketType.REQ);
 
+            worker.connect("tcp://localhost:5671");
+
+            int total = 0;
+            while (true) {
+                //  Tell the broker we're ready for work
+                worker.send("Hi Boss");
+
+                //  Get workload from broker, until finished
+                String workload = worker.recvStr();
+                boolean finished = workload.equals("Fired!");
+                if (finished) {
+                    System.out.printf("Completed: %d tasks\n", total);
+                    break;
+                }
+                total++;
+
+                //  Do some random work
+                try {
+                    Thread.sleep(rand.nextInt(500) + 1);
+                }
+                catch (InterruptedException e) {
+                }
+            }
+        }
     }
 }

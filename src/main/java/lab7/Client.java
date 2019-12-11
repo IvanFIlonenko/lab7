e9b5773827cae5511dcba6240d7799ae8d43252d
Ivan <Ivan Filonenko>
2019-12-11 20:18:09 +0300

    Scripted auto-commit on change (2019-12-11 20:18:09) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -1,42 +1,49 @@
-package lab7;
-
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
 
-import java.util.Random;
 import java.util.Scanner;
 
-public class Client {
-    private static Random rand        = new Random();
-    public static void main(String[] args){
-        try (ZContext context = new ZContext()) {
+static class Client implements Runnable
+{
+    private static int SAMPLE_SIZE = 10000;
+
+    @Override
+    public void run()
+    {
+        try (ZContext ctx = new ZContext()) {
+            ZMQ.Socket client = ctx.createSocket(SocketType.REQ);
+            client.setHWM(0);
+            client.setIdentity("C".getBytes(ZMQ.CHARSET));
+            client.connect("tcp://localhost:5555");
+            System.out.println("Setting up test");
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            int requests;
+            long start;
+
+            System.out.println("Synchronous round-trip test");
+            start = System.currentTimeMillis();
+
             Scanner in = new Scanner(System.in);
-            ZMQ.Socket worker = context.createSocket(SocketType.REQ);
-
-            worker.connect("tcp://localhost:5671");
-
-            int total = 0;
             while (true) {
-                //  Tell the broker we're ready for work
-                //String s = in.nextLine();
-                worker.send("Hi Boss");
-
-                //  Get workload from broker, until finished
-                String workload = worker.recvStr();
-                boolean finished = workload.equals("Fired!");
-                if (finished) {
-                    System.out.printf("Completed: %d tasks\n", total);
+                String message = in.nextLine();
+                if (message.equals("Stop")) {
                     break;
                 }
-                total++;
-
-                //  Do some random work
-                try {
-                    Thread.sleep(rand.nextInt(500) + 1);
-                }
-                catch (InterruptedException e) {
-                }
+                ZMsg req = new ZMsg();
+                req.addString(message);
+                req.send(client);
+                ZMsg ans = ZMsg.recvMsg(client);
+                String s = ans.popString();
+                System.out.println(s);
+                ans.destroy();
             }
         }
     }

887714c02707736148060de596a44afb8c9e5186
Ivan <Ivan Filonenko>
2019-12-11 14:31:19 +0300

    Scripted auto-commit on change (2019-12-11 14:31:19) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -9,7 +9,7 @@ import org.zeromq.ZContext;
 
 public class Client extends Thread{
     private static Random rand        = new Random();
-    public static void main(String[] args){
+    public static void run(String[] args){
         try (ZContext context = new ZContext()) {
             Socket worker = context.createSocket(SocketType.DEALER);
 

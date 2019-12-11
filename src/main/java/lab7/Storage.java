f3a0b4307dc191967d55687cd4be3fd2a77fc1bc
Ivan <Ivan Filonenko>
2019-12-11 20:21:26 +0300

    Scripted auto-commit on change (2019-12-11 20:21:26) by gitwatch.sh

--- src/main/java/lab7/Storage.java
+++ src/main/java/lab7/Storage.java
@@ -1,4 +1,26 @@
 package lab7;
 
-public class Storage {
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import org.zeromq.ZMsg;
+
+static class Worker implements Runnable
+{
+    @Override
+    public void run()
+    {
+        try (ZContext ctx = new ZContext()) {
+            ZMQ.Socket worker = ctx.createSocket(SocketType.DEALER);
+            worker.setHWM(0);
+            worker.setIdentity("W".getBytes(ZMQ.CHARSET));
+            worker.connect("tcp://localhost:5556");
+            while (!Thread.currentThread().isInterrupted()) {
+                ZMsg msg = ZMsg.recvMsg(worker);
+                //String s = msg.popString();
+                System.out.println(msg);
+                msg.send(worker);
+            }
+        }
+    }
 }

11027cfa5f1b066cd3571b946e2ee9d90b419b42
Ivan <Ivan Filonenko>
2019-12-11 14:24:12 +0300

    Scripted auto-commit on change (2019-12-11 14:24:12) by gitwatch.sh

--- src/main/java/lab7/Client.java
+++ src/main/java/lab7/Client.java
@@ -11,8 +11,8 @@ public class Client {
     private static Random rand        = new Random();
     public static void main(String[] args){
         try (ZContext context = new ZContext()) {
-            Scanner in = new Scanner(System.in);
-            ZMQ.Socket worker = context.createSocket(SocketType.REQ);
+            Socket worker = context.createSocket(SocketType.DEALER);
+            ZHelper.setId(worker); //  Set a printable identity
 
             worker.connect("tcp://localhost:5671");
 
@@ -20,25 +20,24 @@ public class Client {
             while (true) {
                 //  Tell the broker we're ready for work
                 worker.sendMore("");
-                String s = in.nextLine();
-                worker.send(s);
+                worker.send("Hi Boss");
 
                 //  Get workload from broker, until finished
-                String reply = worker.recvStr();
-                System.out.println(reply);
-//                boolean finished = workload.equals("Fired!");
-//                if (finished) {
-//                    System.out.printf("Completed: %d tasks\n", total);
-//                    break;
-//                }
-//                total++;
+                worker.recvStr(); //  Envelope delimiter
+                String workload = worker.recvStr();
+                boolean finished = workload.equals("Fired!");
+                if (finished) {
+                    System.out.printf("Completed: %d tasks\n", total);
+                    break;
+                }
+                total++;
 
                 //  Do some random work
-//                try {
-//                    Thread.sleep(rand.nextInt(500) + 1);
-//                }
-//                catch (InterruptedException e) {
-//                }
+                try {
+                    Thread.sleep(rand.nextInt(500) + 1);
+                }
+                catch (InterruptedException e) {
+                }
             }
         }
     }

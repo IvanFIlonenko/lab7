8852eb8ef62e22d5c83bfa78fc2fa9f272faa991
Ivan <Ivan Filonenko>
2019-12-12 18:06:00 +0300

    Scripted auto-commit on change (2019-12-12 18:06:00) by gitwatch.sh

--- src/main/java/lab7/Main.java
+++ src/main/java/lab7/Main.java
@@ -41,6 +41,7 @@ public class Main
                         break; // Interrupted
                     //ZFrame address = msg.pop();
                     //address.destroy();
+                    ZFrame z = msg.unwrap();
                     Object[] arr = msg.toArray();
                     String s = (String)arr[2];
                     for (Map.Entry<ZFrame, Pair<Integer,Integer>> entry : storages.entrySet()){
